452482acd8fdd474600fd1b6e1eef1e02762d329
Thiago Vasconcelos Braga <thiagovb46@gmail.com>
2023-10-15 15:05:26 -0300

    AlteraTestes

--- perceptron.py
+++ perceptron.py
@@ -12,20 +12,30 @@ classes_conjunto = classes[classes != 0]
 classes_conjunto = np.where(classes_conjunto == 1, 1, -1)
 
 taxa_aprendizado = 0.1;
+epoch = 100
 
-for cont in range(3):
+for cont in range(6):
     if cont == 0:
-        taxa_aprendizado = 0.1
         proporcao_conn_teste = 0.1
     else:
         if cont == 1:
-            taxa_aprendizado = 0.2
             proporcao_conn_teste = 0.3
         else: 
             if cont == 2:
-                taxa_aprendizado = 0.3
                 proporcao_conn_teste = 0.5
-    # Dividir o conjunto de dados em treinamento e teste
+            if cont == 3:
+                proporcao_conn_teste = 0.5
+                taxa_aprendizado = 0.2
+                epoch = 100
+            if cont == 4:
+                proporcao_conn_teste = 0.5
+                taxa_aprendizado = 0.3
+                epoch = 100
+            if cont == 5:
+                proporcao_conn_teste = 0.5
+                taxa_aprendizado = 0.5
+                epoch = 100
+    # Divisão entre treinamento e teste
     atributos_treinamento, atributos_teste, classes_treinamento, classes_teste = train_test_split(atributos_do_conjunto, classes_conjunto, test_size=proporcao_conn_teste, random_state=42)
     tamanho_conjunto = atributos_teste.shape[0]
     #Inicialização pesos/bias
@@ -33,9 +43,7 @@ for cont in range(3):
     pesos = np.random.rand(atributos_treinamento.shape[1])
     viés = np.random.rand()
 
-    # Treino com 100 interações
-    maximo_interacaoes = 100
-    for epoca in range(maximo_interacaoes):
+    for epoca in range(epoch):
         for i in range(atributos_treinamento.shape[0]): #Tamanho conjunto treinamento
             entrada = atributos_treinamento[i]
             objetivo = classes_treinamento[i]
@@ -57,8 +65,9 @@ for cont in range(3):
             corretos += 1
 
     acuracia = (corretos / tamanho_conjunto) * 100 #Quantidade de acertos/tamanho do conjunto
-    print("Conjunto "+str(cont+1))
+    print("Teste "+str(cont+1))
     print("     Taxa de aprendizado:", taxa_aprendizado)
     print("     Tamanho conjunto de teste:", proporcao_conn_teste)
+    print("     Epoch              :", epoch)
     print("     Acurácia no conjunto de teste %:", acuracia)
     print("---------------------------------------")
